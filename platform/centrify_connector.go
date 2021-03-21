bf307b495e63190db3ec017bab9b97ffbb865467
Marco <marco.zhang@centrify.com>
2021-03-21 15:12:49 +0800

    Add connector status as query filter

--- platform/centrify_connector.go
+++ platform/centrify_connector.go
@@ -7,6 +7,21 @@ import (
 // Connector - Encapsulates a single Connector
 type Connector struct {
 	vaultObject
+
+	MachineName           string `json:"MachineName,omitempty" schema:"machine_name,omitempty"`
+	SSHService            string `json:"SSHService,omitempty" schema:"ssh_service,omitempty"`
+	RDPService            string `json:"RDPService,omitempty" schema:"rdp_service,omitempty"`
+	ADProxy               string `json:"ADProxy,omitempty" schema:"ad_proxy,omitempty"`
+	AppGateway            string `json:"AppGateway,omitempty" schema:"app_gateway,omitempty"`
+	HttpAPIService        string `json:"HttpAPIService,omitempty" schema:"http_api_service,omitempty"`
+	LDAPProxy             string `json:"LDAPProxy,omitempty" schema:"ldap_proxy,omitempty"`
+	RadiusService         string `json:"RadiusService,omitempty" schema:"radius_service,omitempty"`
+	RadiusExternalService string `json:"RadiusExternalService,omitempty" schema:"radius_external_service,omitempty"`
+	Online                bool   `json:"Online,omitempty" schema:"online,omitempty"`
+	Version               string `json:"Version,omitempty" schema:"version,omitempty"`
+	VpcIdentifier         string `json:"VpcIdentifier,omitempty" schema:"vpc_identifier,omitempty"`
+	VmIdentifier          string `json:"VmIdentifier,omitempty" schema:"vm_identifier,omitempty"`
+	Status                string `json:"-"` // Used to represent Online status
 }
 
 // NewConnector is a Connector constructor
@@ -17,7 +32,7 @@ func NewConnector(c *restapi.RestClient) *Connector {
 	return &s
 }
 
-// Read function fetches a ManaulSet from source, including attribute values. Returns error if any
+// Read function fetches a Connector from source, including attribute values. Returns error if any
 func (o *Connector) Read() error {
 	return nil
 }
@@ -37,6 +52,22 @@ func (o *Connector) Query() (map[string]interface{}, error) {
 	query := "SELECT * FROM Proxy WHERE 1=1"
 	if o.Name != "" {
 		query += " AND Name='" + o.Name + "'"
+	}
+	if o.Status != "" {
+		if o.Status == "Active" {
+			query += " AND Online=true"
+		} else {
+			query += " AND Online=false"
+		}
+	}
+	if o.Version != "" {
+		query += " AND Version='" + o.Version + "'"
+	}
+	if o.VpcIdentifier != "" {
+		query += " AND VpcIdentifier='" + o.VpcIdentifier + "'"
+	}
+	if o.VpcIdentifier != "" {
+		query += " AND VmIdentifier='" + o.VmIdentifier + "'"
 	}
 
 	return queryVaultObject(o.client, query)
